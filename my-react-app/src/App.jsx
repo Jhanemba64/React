2ba426051bbe70559b25a2768a20e7c38fbe7341
Jhanemba64 <130657727+Jhanemba64@users.noreply.github.com>
2023-10-10 19:45:33 +0200

    exercice 4

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,30 @@
-import PokemonCard from "./components/PokemonCard.jsx"
+import PokemonCard from "./components/PokemonCard.jsx";
 
-function App() { return <div><PokemonCard/></div>;
+function App() {
+  const selectedPokemon = {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  };
+
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+  ];
+
+  return (
+    <div>
+      <PokemonCard pokemon={pokemonList[0]} />
+    </div>
+  );
 }
 
 export default App;
